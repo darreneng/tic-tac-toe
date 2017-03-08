e86ba4c307b475671c33b5b316b67b5004d7002d
Darren Eng <darrenle9@gmail.com>
2017-03-08 14:02:31 -0800

    app logic works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './css/App.css';
 // import Board from './Board'
 
+const modeToLabel = {
+  "hvh": "Human vs Human",
+  "hvc": "Human vs CPU",
+  "cvc": "CPU vs CPU"
+}
+
 const Box = (props) => (
   <button className="box" onClick={() => props.onClick()}>
     {props.value}
@@ -40,8 +46,8 @@ class Menu extends Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    this.props.newGame();
-    alert("You have selected mode: " + this.state.mode + "and grid size: " + this.state.grid);
+    this.props.newGame(this.state.mode, this.state.grid);
+    // alert("You have selected mode: " + this.state.mode + " and grid size: " + this.state.grid);
   }
   render() {
     return (
@@ -92,44 +98,88 @@ function checkWinner(boxes, r, c) {
   // check diagonal
   if (r === c && boxes.map((row, i) => row[i]).every(checkVal))
     return val;
+  // check other diagonal
+  const li = boxes.length - 1
+  if (r + c === li && boxes.map((row, i) => row[li - i]).every(checkVal))
+    return val;
 
   return null;
+}
+
+const GameStatus = (props) => {
+  return (
+    <div>
+      <h2>{modeToLabel[props.mode]}</h2>
+      <h4>{(props.isOTurn ? "O" : "X") + "'s turn"}</h4>
+      <h3>{props.hasWinner ? props.hasWinner + " has won!" : ""}</h3>
+    </div>
+  );
 }
 
 class Game extends Component {
   constructor(props) {
     super(props);
-    const dim = 3;
-    this.state = {
-      boxes: Array(dim).fill(Array(dim).fill(null)),
-      isXTurn: true,
-      hasWinner: null
-    };
+    this.state = this.initialState("hvh", 3)
     //this.handleOnClick.bind(this)
   }
   handleOnClick(r, c) {
+    if (this.state.boxes[r][c] || this.state.hasWinner)
+      return;
+
+    this.nextTurn(r, c);
+  }
+  nextTurn(r, c) {
     let boxes = this.state.boxes.slice();
-
-    if (boxes[r][c] || this.state.hasWinner) return;
-
     let row = boxes[r].slice();
-    row[c] = this.state.isXTurn ? 'X' : 'O';
+    row[c] = this.state.turnCount % 2 ? 'O' : 'X'
     boxes[r] = row;
-
     this.setState({
       boxes: boxes,
-      isXTurn: !this.state.isXTurn,
-      hasWinner: checkWinner(boxes, r, c)
+      hasWinner: checkWinner(boxes, r, c),
+      turnCount: this.state.turnCount + 1
     });
   }
-  handleNewGame() {
-    // TODO implement this
+  cpuMove() {
+    const length = this.state.boxes.length;
+    const cpuAgent = () => {
+      while (true) {
+        const r = Math.floor(Math.random() * length);
+        const c = Math.floor(Math.random() * length);
+        if (!this.state.boxes[r][c])
+          return [r,c]
+      }
+    }
+    const next = cpuAgent()
+    setTimeout(() => this.nextTurn(next[0], next[1]), 2000)
+  }
+  initialState(mode, grid) {
+    return {
+      boxes: Array(grid).fill(Array(grid).fill(null)),
+      turnCount: 0,
+      hasWinner: null,
+      mode: mode
+    };
+  }
+  componentDidUpdate() {
+    // make CPU move if necessary
+    const turnCount = this.state.turnCount;
+    const maxc = this.state.boxes.length * this.state.boxes.length;
+    const mode = this.state.mode;
+    if (!this.state.hasWinner && turnCount < maxc &&
+        (mode === "cvc" || (mode === "hvc" && turnCount % 2))) {
+      this.cpuMove();
+    }
+  }
+  handleNewGame(mode, grid) {
+    this.setState(this.initialState(mode, grid));
   }
   render() {
     return (
-      <div>
-        <Menu newGame={() => this.handleNewGame()}/>
+      <div className="container">
+        <Menu newGame={(m,g) => this.handleNewGame(m,g)}/>
         <Board boxes={this.state.boxes} onClick={(r,c) => this.handleOnClick(r,c)}/>
+        <GameStatus isOTurn={this.state.turnCount % 2} mode={this.state.mode}
+                    hasWinner={this.state.hasWinner} />
       </div>
     );
   }
